8763028059b8e54bad4862db9f6b2c9993fab2de
Ming <1197592193@qq.com>
2024-10-11 16:08:20 +0800

    普通更新

--- cinrad/__init__.py
+++ cinrad/__init__.py
@@ -5,6 +5,6 @@ from . import calc
 from . import visualize
 from . import correct
 
-from .io import read_level2
+from .io import read_level2, Standard_X_PUP
 
-__version__ = "1.9.1"
+__version__ = "1.9.2"
