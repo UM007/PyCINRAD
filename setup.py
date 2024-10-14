160a09c2f3402e5c255682626359d418c370fe52
Ming <1197592193@qq.com>
2024-10-14 15:25:30 +0800

    更新raster命名错误

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ data_pth = join("cinrad", "data")
 
 setup(
     name="cinrad",
-    version="1.9.2.2",
+    version="1.9.2.3",
     description="Decode CINRAD radar data and visualize",
     long_description="Decode CINRAD radar data and visualize",
     license="GPL Licence",
